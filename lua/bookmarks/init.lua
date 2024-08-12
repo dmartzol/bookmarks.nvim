91129924deb2df5144780da6981d13522345d146
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2024-08-12 01:13:49 -0500

    first implementation

--- lua/bookmarks/init.lua
+++ lua/bookmarks/init.lua
@@ -1,7 +1,57 @@
+local M = {}
+
+-- array to store line numbers and column numbers
 local bookmarks = {}
 
-bookmarks.setup = function(opts)
-    print("Options: ", opts)
+-- Define the sign for bookmarks
+vim.fn.sign_define("Explosion", { text = "💥", texthl = "SignColumn" })
+vim.fn.sign_define("Bookmark", { text = "⭕", texthl = "SignColumn" })
+
+-- sign_place({id}, {group}, {name}, {buf} [, {dict}])
+M.add = function()
+	local currentPosition = vim.api.nvim_win_get_cursor(0)
+	local line, col = currentPosition[1], currentPosition[2]
+	local sign_id = vim.fn.sign_place(0, "bookmark", "Bookmark", 0, { lnum = line, priority = 10 })
+	table.insert(bookmarks, { line, col, id = sign_id })
 end
 
-return bookmarks
+M.remove = function()
+	local currentPosition = vim.api.nvim_win_get_cursor(0)
+	local line = currentPosition[1]
+	for i, pos in ipairs(bookmarks) do
+		if pos[1] == line then
+			table.remove(bookmarks, i)
+			vim.fn.sign_unplace("Bookmark", { buffer = 0, id = 0 })
+			return
+		end
+	end
+end
+
+M.is_bookmarked = function()
+	local currentPosition = vim.api.nvim_win_get_cursor(0)
+	local line = currentPosition[1]
+	for _, pos in ipairs(bookmarks) do
+		if pos[1] == line then
+			return true
+		end
+	end
+	return false
+end
+
+M.toggle = function()
+	if M.is_bookmarked() then
+		M.remove()
+	else
+		M.add()
+	end
+end
+
+M.print_all = function()
+	for i, pos in ipairs(bookmarks) do
+		print("Position " .. i .. ": Row " .. pos[1] .. ", Column " .. pos[2])
+	end
+end
+
+M.setup = function() end
+
+return M
